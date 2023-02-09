cf1b62ae9bf1b2b9f049e325bcc56246c1b15e09
rydlande <rydlande@hotmail.com>
2023-02-09 11:04:20 +0100

    made changes from async to then/catch. Did som organizing. Worked on errors, but that did not work

--- API/api.js
+++ API/api.js
@@ -3,12 +3,13 @@ const url = "https://api.spaceflightnewsapi.net/v3/articles";
 const main = document.querySelector("main");
 
 
-const getArticles = async () => {
-    const res = await fetch(url);        //før du går videre, vent til den har hentet url
-    console.log(res);                    //får et løfte om å få et svar/at noe skal bli gjort "promise"
+const getArticles = (data) => {                               //async (await) - async gjør ingenting, men gjør at vi kan bruke await (vi byttet den ut med fetch senere)
+    /*const res = await fetch(url);                           //await - før du går videre, vent til den har hentet url
+    console.log(res);                                         //får et løfte om å få et svar/at noe skal bli gjort "promise"
     const data = await res.json();
-    console.log(data);
-    data.forEach((element) => {
+    console.log(data);*/
+
+    data.forEach((element) => {                               //forEach - går gjennom hvert element i et array
         /*
         const container = document.createElement("div");
         container.classList.add("container");
@@ -30,13 +31,14 @@ const getArticles = async () => {
         author.innerHTML = `Author: ${obj.newsSite}`;
         container.appendChild(author);
         */
-        const { title, imageUrl, newsSite, summary, url, } = element;
+       
+        const { title, imageUrl, newsSite, summary, url, publishedAt} = element;   //objects destructuring
         const container = document.createElement("div");
         container.classList.add("container");
         container.innerHTML = `
         <div class="card--header">
           <img
-            src="${imageUrl}"
+            src="${imageUrl}"                                   //henter en variabel fra en 
             alt="${title} ${newsSite}"
           />
         </div>
@@ -47,17 +49,18 @@ const getArticles = async () => {
           </div>
           <div class="card--body__details">
             <p><span class="author">Author:</span> ${newsSite}</p>
+          
             <a href="${url}" target="_blank">Read More</a>
           </div>
         </div>`
 
-        /*
+        /* DATO FØRSTE FORSØK (millisekund)
         const date = document.createElement("date");
         date.src = Date.parse(element.publishedAt);
         console.log(date.src);
         */
 
-
+        /* DATO ANDRE FORSØK (fungerer ish, men dårlig)
         let utcDate = new Date(element.publishedAt);
         let localDate = new Date(
             utcDate.getFullYear(),
@@ -67,7 +70,14 @@ const getArticles = async () => {
             utcDate.getMinutes(),
             utcDate.getSeconds()
          );
-         console.log(localDate);
+         console.log(localDate);*/
+
+         const d = new Date(publishedAt).toLocaleDateString('en-EU', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          });
+          console.log(d);
 
 
 
@@ -75,4 +85,17 @@ const getArticles = async () => {
     });
 };
 
-getArticles();
+
+//getArticles();            //trenger ikke den når vi bruker en fetch
+
+
+fetch(url)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    getArticles(data);
+  })
+  .catch((e) => {
+    main.innerHTML = `Sorry, there seems to been a mistake - please try again later`
+  });
