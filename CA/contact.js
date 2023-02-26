2b66451537b53f4d28c7c47a077ed7c349efb006
rydlande <rydlande@hotmail.com>
2023-02-26 23:48:21 +0100

    worked with forms on contact page

--- CA/contact.js
+++ CA/contact.js
@@ -1,6 +1,21 @@
-const name = document.getElementById("name");
-const subject = document.getElementById("subject");
-const email = document.getElementById("email");
-const adress = document.getElementById("adress");
+const name = document.getElementById("name").addEventListener("invalid", nameFunc);
+const subject = document.getElementById("subject").addEventListener("invalid", subjectFunc);
+const email = document.getElementById("email").addEventListener("invalid", emailFunc);
+const adress = document.getElementById("adress").addEventListener("invalid", adressFunc);
+const submit = document.getElementById("submit").submit(); 
 
+function nameFunc() {
+  alert("Required");
+}
 
+function subjectFunc() {
+    alert("Must have at least 10 characteres");
+  }
+
+function emailFunc() {
+  alert("Must be an e-mail adress");
+}
+
+function adressFunc() {
+    alert("Must have at least 25 characteres");
+  }
