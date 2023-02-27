ddfddaf0a01e5e79ac903c07f4857ebe807d200f
rydlande <rydlande@hotmail.com>
2023-02-27 22:44:23 +0100

    made the event form to work. Styled it. All done

--- contact.js
+++ contact.js
@@ -1,33 +1,43 @@
-const name = document.getElementById("name");
-const subject = document.getElementById("subject").addEventListener("invalid", subjectFunc);
-const email = document.getElementById("email").addEventListener("invalid", emailFunc);
-const adress = document.getElementById("adress").addEventListener("invalid", adressFunc);
-const submit = document.getElementById("submit").submit(); 
-const btn = document.getElementById
+const form = document.querySelector("form");
+form.addEventListener("submit", validateForm);
+
+const fullName = document.querySelector("input#name");
+const subject = document.querySelector("input#subject");
+const email = document.querySelector("input#email");
+const adress = document.querySelector("input#adress");
 
 
+const nameMessage = document.querySelector("#nameMessage");
+const subjectMessage = document.querySelector("#subjectMessage");
+const emailMessage = document.querySelector("#emailMessage");
+const adressMessage = document.querySelector("#adressMessage");
 
 
+function validateForm(e) {
+  e.preventDefault();
 
-function nameFunc() {
-  alert("Required");
+      let subName = fullName.value.trim();
+      nameMessage.innerHTML = "";
+      if (subName.length == 0) {
+        nameMessage.innerHTML = "Requried";
+      }
+    
+      let subSubject = subject.value.trim();
+      subjectMessage.innerHTML = "";
+      if (subSubject.length < 10) {
+        subjectMessage.innerHTML = "Subject must be at least 10 charecters long";
+      }
+
+      let subEmail = email.value.trim();
+      emailMessage.innerHTML = "";
+      let emailPattern = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+      if (!emailPattern.test(subEmail)) {
+        emailMessage.innerHTML = "Please enter a valid email";
+      }
+
+      let subAdress = adress.value.trim();
+      adressMessage.innerHTML = "";
+      if (subAdress.length < 25) {
+        adressMessage.innerHTML = "Please tell us a bit more. At least 25 characters";
+      }
 }
-
-subjectFunc.onkeyup = () => {
-  let inLen = subjectFunc.value.length;
-  console.log(inLen);
-
-    alert("Must have at least 10 characteres");
-  }
-
-function emailFunc() {
-  alert("Must be an e-mail adress");
-}
-
-function adressFunc() {
-    alert("Must have at least 25 characteres");
-  }
-
-
-btn.addEventListener("click", submit)
-//submit.onclick = function ()
